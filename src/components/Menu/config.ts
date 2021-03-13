5d6556b6308975f24fac024d649a68489e16b3f5
bboynam99 <30984515+bboynam99@users.noreply.github.com>
2021-03-13 10:08:00 +0700

    update

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,74 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: 'Exchange',
+        href: 'https://exchange.rockswap.finance/',
+      },
+      {
+        label: 'Liquidity',
+        href: 'https://exchange.rockswap.finance/#/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: '/farms',
+  },
+  {
+    label: 'Mining',
+    icon: 'PoolIcon',
+    href: '/mining',
+  },
+  // {
+  //   label: 'Pools',
+  //   icon: 'PoolIcon',
+  //   href: '/pools',
+  // },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: '/lottery',
+  // },
+  // {
+  //   label: 'NFT',
+  //   icon: 'NftIcon',
+  //   href: '/nft',
+  // },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'PancakeSwap',
+        href: 'https://pancakeswap.info/token/0x5f26be59d28e0b3a9c58e242a58acc05e000f4ad',
+      },
+    ],
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: 'Github',
+        href: 'https://github.com/rockswapfinance/',
+      },
+      {
+        label: 'Blog',
+        href: 'https://rockswap.medium.com/',
+      },
+    ],
+  },
+]
+
+export default config
